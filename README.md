d5d54baa9728ad876f405dd3fe7a31437c26735a
Kirsten Göbel <kirsten-goebel@gmx.de>
2023-12-04 16:41:15 +0100

    day 4 tidy

--- README.md
+++ README.md
@@ -4,9 +4,9 @@ https://adventofcode.com/2023
 | Day | Name        | Code | Input |
 |-----|-------------|------|-------|
 | 01  | Trebuchet?! | [Python](scripts/trebuchet.py)   |    [Input](input/trebuchet.txt)   |
-| 02  | Cube Conundrum             | [Python](scripts/cube_conundrum.py)     |  [Input](cube_conundrum.txt)     |
-| 03  |             |      |       |
-| 04  |             |      |       |
+| 02  | Cube Conundrum             | [Python](scripts/cube_conundrum.py)     |  [Input](input/cube_conundrum.txt)     |
+| 03  | Gear Ratios            | [Python](scripts/gear_ratios.py)     |   [Input](input/gear_ratios.txt)    |
+| 04  |   Scratchcards          |  [Python](scripts/scratchcards.py)    |  [Input](input/scratchcards.txt)     |
 | 05  |             |      |       |
 |     |             |      |       |
 |     |             |      |       |
--- scripts/scratchcards.py
+++ scripts/scratchcards.py
@@ -1,42 +1,55 @@
 #!/usr/bin/env python
 
-import math
 import re
-data = open("input/scratchcards.txt").read().splitlines()
+import sys
 
-results=[]
+def get_cards(path:str="input/scratchcards.txt") -> dict:
+    data = open(path).read().splitlines()
 
-queue= []
-values={}
+    values:dict[int, int] = {}
 
-for line in data:
-    cards, numbers = line.split(":")
-    cards = re.findall("[0-9]{1,}", cards)
-    winning, yours = numbers.split("|")
-    winning = [x for x in winning.strip().split(" ") if not x == ""]
-    yours = [x for x in yours.strip().split(" ") if not x == ""]
-    
-    winning = set(winning)
-    yours = set(yours)
-    
-    num_match = len(winning.intersection(yours))
-    queue.append((int(cards[0]), num_match))
-    values[int(cards[0])] = num_match
-    points = 2**(num_match-1) if num_match > 0 else 0
-    results.append(points)
-    
-print("Part 1", sum(results))
-
-results=[]
-
-cards = 0
-
-while queue:
-    card, val = queue.pop()
-    
-    for i in range(card+1, card+val+1):
-        queue.append((i, values[i]))
-
-    cards+=1
+    for line in data:
+        cards, numbers = line.split(":")
         
-print("Part 2", cards)
+        card:str = re.findall("[0-9]{1,}", cards)[0]
+        
+        winner, your_deck = numbers.split("|")
+        
+        winner:list = set([x for x in winner.strip().split(" ") if not x ==""])
+        your_deck:list = set([x for x in your_deck.strip().split(" ") if not x ==""])
+        
+        overlaps:int = len(winner.intersection(your_deck))
+        values[int(card)] = overlaps
+        
+    return values
+        
+def get_winning_numbers(values:dict) -> int:
+    points:int = 0
+    
+    for _, point in values.items():
+        points += 2**(point-1) if point > 0 else 0
+        
+    return points
+
+def get_number_cards(values:dict) -> int:
+    queue:list = [(key, value) for key, value in values.items()]
+    
+    cards:int = 0
+    
+    while queue:
+        card, val = queue.pop()
+    
+        for i in range(card+1, card+val+1):
+            queue.append((i, values[i]))
+
+        cards+=1
+    
+    return cards
+        
+def main(): 
+    data = get_cards()
+    print("Part 1", get_winning_numbers(data))
+    print("Part 2", get_number_cards(data))
+
+if __name__ == "__main__":
+    main()
