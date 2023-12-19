2a7b8fa067359515e3b984c6732a9ba1ecd0ac4a
Kirsten Göbel <kirsten-goebel@gmx.de>
2023-12-19 17:36:20 +0100

    part 2 day 18

--- scripts/lavaduct_lagoon.py
+++ scripts/lavaduct_lagoon.py
@@ -1,135 +1,60 @@
 #!/usr/bin/env python
 
+# initially adapted day 10 for part 1, but after getting an idea for part two, switched completely to shoelace and pick's theorem
+
 data = open("input/lavaduct_lagoon.txt").read().splitlines()
 
 data = [tuple(x.split(" ")) for x in data]
+part1 = [(x[0], int(x[1])) for x in data]
 
-curr = (0,0)
+directions = {"0":"R", "1":"D", "2":"L", "3":"U"}
+part2 = [(directions[x[2][-2]], int(x[2][2:-2], 16)) for x in data]
 
-border=[curr]
+def get_borders(data:tuple[str, tuple[int, int]]) -> list[int]:
+    curr = (0,0)
+    
+    points:list = []
+    length:int = 0
+    for direction, steps in data:
+        y, x = curr  #row, gollum
+        length+=steps
+        
+        match direction:
+            case "R":
+                curr = (y, x+steps)
+            case "L":
+                curr = (y, x-steps)
+            case "D":
+                curr = (steps+y, x)
+            case "U":
+                curr = (y-steps, x)
+                
+        points.append(curr)
+        
+    return tuple(points), length
 
-direc = {"0":"R", "1":"D", "2":"L", "3":"U"}
+def shoelace(polygonBoundary, absoluteValue = True):
+    nbCoordinates = len(polygonBoundary)
+    nbSegment = nbCoordinates - 1
 
-part2 = []
-for direction, steps, color in data:
-    y, x = curr  #row, gollum
-    x, y = int(x), int(y)
-    steps = int(steps)
+    l = [(polygonBoundary[i+1][0] - polygonBoundary[i][0]) * (polygonBoundary[i+1][1] + polygonBoundary[i][1]) for i in range(nbSegment)]
 
-    match direction:
-        case "R":
-            for step in range(x+1, x+steps+1):
-                border.append((y, step))
-            curr = (y, step)
-        case "L":
-            for step in range(x-steps, x):
-                border.append((y, step))
-            curr = (y, x-steps)
-        case "D":
-            for step in range(y+1, y+steps+1):
-                border.append((step, x))
-            curr = (step, x)
-        case "U":
-            for step in range(y-steps, y):
-                border.append((step, x))
-            curr = (y-steps, x)
-           
-    part2.append((direc[color[-2]], int(color[2:-2], 16)))
-            
-# build grid
-maxrow = max([x for x, y in border])
-maxgol = max([y for x, y in border])
-minrow = min([x for x, y in border])
-mingol = min([y for x, y in border])
-rowshift=0
-golshift=0
-
-if minrow < 0:
-    rowshift=abs(minrow)
-if mingol < 0:
-    golshift=abs(mingol)
-
-border = list(set(border))
-border = [(row+rowshift, gol+golshift) for row, gol in border]
-
-shift=2
-grid=[["x" for gol in range(min(0, mingol-shift),maxgol+shift+1)] for row in range(min(0, minrow-shift), maxrow+shift+1)]
-
-for row, gollum in border:
-    if (row+1, gollum) in border and (row, gollum+1) in border:
-        grid[row+shift][gollum+shift] = "F"
-    elif (row-1, gollum) in border and (row, gollum+1) in border:
-        grid[row+shift][gollum+shift] = "L"
-    elif (row+1, gollum) in border and (row, gollum-1) in border:
-        grid[row+shift][gollum+shift] = "7"
-    elif (row-1, gollum) in border and (row, gollum-1) in border:
-        grid[row+shift][gollum+shift] = "J"
-    elif (row-1, gollum) in border and (row+1, gollum) in border:
-        grid[row+shift][gollum+shift] = "|"
-    elif (row, gollum-1) in border and (row, gollum+1) in border:
-        grid[row+shift][gollum+shift] = "-"
+    if absoluteValue:
+        return abs(sum(l) / 2.)
     else:
-        print("WTF")
-        
-border = [(row+shift, gol+shift) for row, gol in border]
+        return sum(l) / 2.
 
-#P art 1
-inside=0
-for row, line in enumerate(grid):
-    for gollum, char in enumerate(line):
-        if (row, gollum) in border: # do not check for element in pipe
-            continue 
+def picks(area, points) -> int:
+    return area - points/2 + 1
 
-        inside += sum([line[:gollum].count("J"), line[:gollum].count("|"), line[:gollum].count("L")]) % 2 == 1 # uneven counts of my elements means my dot is inside
-        
-print("Part 1", inside+len(set(border)))
+p1, length = get_borders(part1)
+p1 = p1[::-1]
 
+area=shoelace(p1)
+print("Part 1", int(picks(area, length)+length))
 
+p2, length = get_borders(part2)
+p2 = p2[::-1]
 
-print(part2)
-curr=(0,0)
-border=[curr]
-border_length=0
-for direction, steps in part2:
-    y, x = curr  #row, gollum
-    border_length+=steps
-    match direction:
-        case "R":
-            for step in range(x+1, x+steps+1):
-                pass#border_length+=1
-            curr = (y, step)
-        case "L":
-            for step in range(x-steps, x):
-                border_length+=1
-            curr = (y, x-steps)
-        case "D":
-            for step in range(y+1, y+steps+1):
-                pass#border_length+=1
-            curr = (step, x)
-        case "U":
-            for step in range(y-steps, y):
-                pass#border_length+=1
-            curr = (y-steps, x)
-    border.append(curr)
-
-from itertools import tee
-
-def pairwise(iterable):
-	a, b = tee(iterable)
-	next(b, None)
-	return zip(a, b)
-
-def shoelace(vertices):
-	area = 0
-	for (r1, c1), (r2, c2) in pairwise(vertices):
-		area += (r2 + r1) * (c2 - c1)
-
-	return abs(area) // 2
-
-def solve(vertices, perimeter):
-	area = shoelace(vertices)
-	return int(area - perimeter / 2 + 1) + perimeter
-
-border = border[:-1]#[::-1]
-
-print(solve(border, border_length))
+area=shoelace(p2)
+print("Part 2", int(picks(area, length)+length))
